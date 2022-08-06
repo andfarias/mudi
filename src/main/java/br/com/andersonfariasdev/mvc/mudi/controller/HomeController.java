a7adac3ba3f03c24c0ea8be8e13a7a5761fb231a
Anderson Farias <andersonsantofarias@outlook.com>
2022-08-06 15:21:50 -0300

    feat: adicão JPA

--- src/main/java/br/com/andersonfariasdev/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/andersonfariasdev/mvc/mudi/controller/HomeController.java
@@ -3,6 +3,10 @@ package br.com.andersonfariasdev.mvc.mudi.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,15 +15,14 @@ import br.com.andersonfariasdev.mvc.mudi.model.Pedido;
 
 @Controller
 public class HomeController {
+	@PersistenceContext
+	private EntityManager entityManager;
+	
 	@GetMapping("/home")
 	public String home(Model model) {
-		Pedido pedido = new Pedido();
-		pedido.setNomeProduto("Monitor gamer AOC G2 27G2HE5 led 27");
-		pedido.setDescricao("Monitor");
-		pedido.setUrlImagem("https://http2.mlstatic.com/D_NQ_NP_2X_604119-MLA47584909551_092021-F.webp");
-		pedido.setUrlProduto("https://www.mercadolivre.com.br/monitor-gamer-aoc-g2-27g2he5-led-27-preto-e-vermelho-100v240v/p/MLB18333881?pdp_filters=category:MLB99245#searchVariation=MLB18333881&position=5&search_layout=grid&type=product&tracking_id=462aaedf-9aa9-4ddf-94ff-4803ad5345d4");
+		Query query = entityManager.createQuery("SELECT p FROM Pedido p", Pedido.class);
 		
-		List<Pedido> pedidos = Arrays.asList(pedido);
+		List<Pedido> pedidos = query.getResultList();
 		model.addAttribute("pedidos", pedidos);
 		
 		return "home";
